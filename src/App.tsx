1e7456ea533a58e7fe398b1bcce143c03ffdc144
Dargon <argon181120@gmail.com>
2026-05-19 14:57:57 +0100

    Enumeraciones y estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 //import BasicTypes from './typescript/BasicTypes';
 // import ObjectLiterals from './typescript/ObjectLiterals';
 // import { BasicFunctions } from './typescript/BasicFunctions';
+//import { Counter } from "./components/Counter";
 
-import { Counter } from "./components/Counter";
+import { LoginPage } from "./components/LoginPage";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
 
   return (
-    <>
-
+    <AuthProvider>
       <div className="flex flex-col justify-center items-center h-svh">
         <h1 className="text-4xl mb-5" >React + TS</h1>
 
         {/* <BasicTypes />   */}
         {/* <ObjectLiterals/> */}
         {/* <BasicFunctions/> */}
-        <Counter/>
+        {/* <Counter/> */}
+        <LoginPage/>
+
       </div>
-    </>
+    </AuthProvider>
   )
 }
 
