8937b173e337c7ff6ad4e0ee0544dd11208411a5
Dargon <argon181120@gmail.com>
2026-06-01 10:00:25 +0100

    Peticiones HTTP

---
+++ src/components/UserRow.tsx
@@ -0,0 +1,17 @@
+export const UserRow = () => {
+
+    return (
+        <tr className="p-2">
+            <td>
+                <img src="" className="rounded-full w-14" alt="User Avatar" />
+            </td>
+            <td>
+                {'Daniel'} {'Artiles'}
+            </td>
+            <td>
+                {'Daniel@correo.com'}
+            </td>
+        </tr>
+
+    )
+}
